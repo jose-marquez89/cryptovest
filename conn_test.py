e58b946acbb6eb30424238c9818c73ca5141e24e
Jose Marquez <jlmarquez89@gmail.com>
2021-03-07 15:48:06 -0600

    adds tests for sqlalchemy connection

---
+++ conn_test.py
@@ -0,0 +1,45 @@
+import os
+
+import pymysql
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, BigInteger, String, Float, ForeignKey
+
+load_dotenv()
+
+DB_UNAME = os.environ["DB_UNAME"]
+DB_PWORD = os.environ["DB_PWORD"]
+DB_HOST = os.environ["DB_HOST"]
+DB_NAME = os.environ["DB_NAME"]
+
+engine = create_engine(f'mysql+pymysql://{DB_UNAME}:{DB_PWORD}@{DB_HOST}/{DB_NAME}')
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(BigInteger, primary_key=True, nullable=False)
+    name = Column(String(32), unique=True)
+    password = Column(String(32))
+    transaction = relationship("Ledger")
+
+    def __repr__(self):
+        return "<User(name={}, password={}>".format(self.name, self.password)
+
+class Ledger(Base):
+    __tablename__ = 'ledger'
+    
+    id = Column(BigInteger, primary_key=True, nullable=False)
+    asset = Column(String(5))
+    amount = Column(Float)
+    user_id = Column(BigInteger, ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return "<Ledger(asset={}, amount={}, user_id={})>".format(self.asset, self.amount,self.user_id)
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
