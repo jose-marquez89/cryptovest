9bbff7855ab8c9a986fcb8f3fe8ebde7895d3bd6
Jose Marquez <jlmarquez89@gmail.com>
2021-03-24 21:53:51 -0500

    renames login.py

---
+++ pages/new_account.py
@@ -0,0 +1,47 @@
+import flask
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+username_group = dbc.FormGroup(
+    [
+        dbc.Label("Username", html_for="username"),
+        dbc.Input(type="email", id="username", placeholder="Enter new username")
+    ]
+)
+
+password_group = dbc.FormGroup(
+    [
+        dbc.Label("Password", html_for="password"),
+        dbc.Input(type="password", id="password", placeholder="Enter a unique password")
+    ]
+)
+
+form = dbc.Col(
+    dbc.Form(
+        [
+            username_group, 
+            password_group,
+            dbc.Button("Submit", color="primary", block=True)
+        ],
+        id="uname-pw-submit",
+        action="/submit",
+        method="POST"
+    ), 
+    md=4
+)
+
+layout = html.Div(
+    [
+        dbc.Row(dbc.Col(html.Br(), md=12)),
+        dbc.Row(
+            [
+                dbc.Col(md=4), 
+                form, 
+                dbc.Col(md=4)
+            ]
+        ),
+        dbc.Row()
+    ]
+)
