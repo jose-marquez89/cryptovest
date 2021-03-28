e0a8105432d050132ff88960f7e1349e84d64693
Jose Marquez <jlmarquez89@gmail.com>
2021-03-28 10:38:28 -0500

    adds proper username and password function

--- pages/new_account.py
+++ pages/new_account.py
@@ -1,33 +1,45 @@
+import logging
+
 import flask
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+from app import app
+
+FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 
 username_group = dbc.FormGroup(
     [
-        dbc.Label("Username", html_for="username"),
-        dbc.Input(type="email", id="username", placeholder="Enter new username")
-    ]
+        dbc.Input(name="username", placeholder="Enter new username")
+    ],
 )
 
 password_group = dbc.FormGroup(
     [
-        dbc.Label("Password", html_for="password"),
-        dbc.Input(type="password", id="password", placeholder="Enter a unique password")
+        dbc.Input(type="password", name="password", placeholder="Enter a unique password")
     ]
 )
 
 form = dbc.Col(
-    dbc.Form(
-        [
-            username_group, 
-            password_group,
-            dbc.Button("Submit", color="primary", block=True)
-        ],
-        id="uname-pw-submit",
-        action="/submit",
-        method="POST"
+    dbc.Card(
+        dbc.CardBody([
+            # for some reason, this does NOT work with dbc.Form
+            # will not hit the /submit route
+            html.H4("New Account"),
+            html.Br(),
+            html.Form(
+                [
+                    username_group,
+                    password_group,
+                    dbc.Button("Submit", id="submit-button", block=True, color="primary")
+                ],
+                id="uname-pw-submit",
+                action="/submit",
+                method="post"
+            )
+        ])
     ), 
     md=4
 )
@@ -39,9 +51,19 @@ layout = html.Div(
             [
                 dbc.Col(md=4), 
                 form, 
-                dbc.Col(md=4)
+                dbc.Col(html.Div(id="submit-message"), md=4)
             ]
         ),
         dbc.Row()
     ]
 )
+
+@app.server.route("/submit", methods=["POST"])
+def submit_new_acc():
+    logging.debug("submit triggered")
+    details = flask.request.form
+    logging.debug(details)
+    username = details.get("username")
+    password = details.get("password")
+
+    return flask.redirect("/") 
