1cca4f93c727356b7c6a1b9128d92cc85542d35d
Jose Marquez <jlmarquez89@gmail.com>
2021-03-28 10:37:28 -0500

    removes unnecessary imports

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 import dash
 import dash_bootstrap_components as dbc
-import dash_html_components as html
-import dash_core_components as dcc
-from dash.dependencies import Input, Output
-from pages import index, new_account
-
-_app_route = "/"
 
 external_stylesheets = [
     dbc.themes.DARKLY,
@@ -19,56 +13,5 @@ meta_tags = [
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
                 meta_tags=meta_tags)
 
-navbar = dbc.NavbarSimple(
-    brand='Crypto Investment Tracker',
-    brand_href='/',
-    children=[
-        dbc.NavItem(dcc.Link('Log In', href='#', className='nav-link')),
-        dbc.NavItem(dcc.Link('Sign Up', href='#', className='nav-link'))
-    ],
-    sticky='top',
-    color='primary',
-    dark=True
-)
-
-footer = dbc.Container(
-    dbc.Row(
-        dbc.Col(
-            html.P([
-                html.Span('Jose Marquez', className='mr-2'),
-                html.A(
-                    html.I(className='fab fa-github-square mr-1'),
-                    href='https://github.com/jose-marquez89'
-                )
-            ],
-            className='lead')
-        )
-    )
-)
-
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    navbar,
-    dbc.Container(id='page-content', className='mt-4'),
-    html.Hr(),
-    footer
-])
-
-app.title = "Crypto Investment Tracker"
-
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/':
-        return index.layout
-    elif pathname == '/new-account':
-        return new_account.layout
-    else:
-        return html.H2("Page Not Found")
-
-@app.server.route("/submit", methods=["POST"])
-def submit_new_acc():
-    details = flask.request.form
-    username = details.get("username")
-    password = details.get("password")
-    return flask.redirect("/") 
+if __name__ == "__main__":
+    app.run_server(debug=True)
